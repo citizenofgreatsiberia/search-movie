29b6f077429293a707a83b7c1b22ff8efa851a6a
Sergey Kostyukevich <myxseriess@outlook.com>
2024-02-24 12:14:03 +0700

    fix mobile layout

--- src/assets/components/reviews/Reviews.jsx
+++ src/assets/components/reviews/Reviews.jsx
@@ -1,3 +1,4 @@
+import Review from '@components/review/Review.jsx'
 import styles from './Reviews.module.css'
 
 const Reviews = ({ reviews }) => {
@@ -8,31 +9,10 @@ const Reviews = ({ reviews }) => {
                 {
                     reviews.map((rev) => {
                         return (
-                            <div key={rev.date} className={styles.review}>
-                                <div className={styles.header}>
-                                    <p className={styles.author}>{rev.author}:</p>
-                                    {
-                                        rev.title ?
-                                            <h3 className={styles.second_title}>"{rev.title}"</h3>
-                                            : null
-                                    }
-                                    {
-                                        rev.type === 'POSITIVE' ?
-                                            <p className={styles.text}>Оценка: хорошо</p>
-                                            : rev.type === 'NEGATIVE' ?
-                                                <p className={styles.text}>Оценка: плохо</p>
-                                                :
-                                                <p className={styles.text}>Оценка: нормально</p>
-                                    }
-                                </div>
-                                <div className={styles.main}>
-                                    <p className={styles.text}>{rev.description.replace(/(\<(\/?[^>]+)>)|&#\d{3,4}?;|&laquo;|&raquo;/g, '')}</p>
-                                </div>
-                            </div>
+                            <Review rev={rev} />
                         )
                     })
                 }
-
             </section>
         )
     } else {
