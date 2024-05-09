ee21e6f58cd0753fd7703236a1d5c3da341b1ef5
Sergey Kostyukevich <76240257+citizenofgreatsiberia@users.noreply.github.com>
2024-05-09 12:50:15 +0700

    Update FilmPage.jsx

--- src/assets/pages/film-page/FilmPage.jsx
+++ src/assets/pages/film-page/FilmPage.jsx
@@ -7,6 +7,7 @@ import FilmProfile from '@components/film-profile/FilmProfile.jsx'
 import Facts from '@components/facts/Facts.jsx'
 import Reviews from '@components/reviews/Reviews.jsx'
 import Similar from '@components/similar/Similar.jsx'
+import Player from '@components/player/Player.jsx'
 
 import styles from './FilmPage.module.css'
 
@@ -20,17 +21,18 @@ export async function loader({ params }) {
     const aboutSimilar = await fetchData(`https://kinopoiskapiunofficial.tech/api/v2.2/films/${filmId}/similars`)
     const streamings = await fetchData(`https://kinopoiskapiunofficial.tech/api/v2.2/films/${filmId}/external_sources?page=1`)
 
-    return { aboutFilm, aboutStuff, aboutFacts, aboutReviews, aboutSimilar, streamings }
+    return { aboutFilm, aboutStuff, aboutFacts, aboutReviews, aboutSimilar, streamings, filmId }
 }
 
 const FilmPage = () => {
-    const { aboutFilm, aboutStuff, aboutFacts, aboutReviews, aboutSimilar, streamings } = useLoaderData()
+    const { aboutFilm, aboutStuff, aboutFacts, aboutReviews, aboutSimilar, streamings, filmId } = useLoaderData()
 
     scrollToTop()
 
     return (
         <div className={styles.wrapper}>
             <FilmProfile aboutFilm={aboutFilm} aboutStuff={aboutStuff} streamings={streamings} />
+            <Player filmId={filmId} />
             <Facts aboutFacts={aboutFacts} />
             <Similar similar={aboutSimilar} />
             <Reviews reviews={aboutReviews?.items} />
