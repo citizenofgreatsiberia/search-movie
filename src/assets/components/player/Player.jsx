39f5725835974d5222e7fdd259928db9dfc6e3ab
Sergey Kostyukevich <76240257+citizenofgreatsiberia@users.noreply.github.com>
2024-05-09 20:19:19 +0700

    Update Player.jsx

--- src/assets/components/player/Player.jsx
+++ src/assets/components/player/Player.jsx
@@ -1,23 +1,17 @@
-import { useEffect } from 'react';
+import { useEffect } from 'react'
 import styles from './Player.module.css'
 
 const Player = ({ filmId }) => {
-    const scriptSrc = "https://kinobox.tv/kinobox.min.js"
 
     useEffect(() => {
-        const kinobox = document.createElement('script')
-        kinobox.src = scriptSrc
-        kinobox.async = true
-        document.body.appendChild(kinobox);
         const kinoboksParams = document.createElement('script')
         kinoboksParams.innerText = `{kbox('.kinobox_player', { search: { kinopoisk: ${filmId} } })}`
         kinoboksParams.async = true
         document.body.appendChild(kinoboksParams)
         return () => {
-            document.body.removeChild(kinobox)
             document.body.removeChild(kinoboksParams)
-        };
-    }, [filmId]);
+        }
+    }, [filmId])
 
     return (
         <section className={styles.wrapper}>
